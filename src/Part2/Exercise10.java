4aae6ee30b69a932cd2beb2da490554bc09f249c
marco.caro <marco.caro@106-05.info2>
2025-10-03 13:35:55 +0200

    Ejercicios Unidad 1 Parte 2

--- src/Part2/Exercise10.java
+++ src/Part2/Exercise10.java
@@ -4,13 +4,20 @@ import java.util.Scanner;
 
 public class Exercise10 {
 public static void main(String[] args) {
+	//Inicializamos el Scanner
 	Scanner sc = new Scanner (System.in);
+	//Declaramos las variable enteras
 	int año;
 	int siglo;
+	//Imprimimos el año
 	System.out.print("Introduce un año: ");
+	//Recogemos en el Scanner el dato
 	año = sc.nextInt();
+	//Damos valor a la variable siglo
 	 siglo = (año / 100) + 1;
+	 //Imprimimos el resultado
 	 System.out.print("Eso es el siglo: " + siglo);
+	 //Cerramos el Scanner
 	 sc.close();
 	
 }
